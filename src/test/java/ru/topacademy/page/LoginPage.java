ffe9a929e88b3bbcda012c8b5c14fe2efdb94cf1
Milana Kichutkina <MilanaVladimir@mail.ru>
2024-09-20 22:14:31 +0500

    fix2 login

--- src/test/java/ru/topacademy/page/LoginPage.java
+++ src/test/java/ru/topacademy/page/LoginPage.java
@@ -1,13 +1,18 @@
 package ru.topacademy.page;
 
+import com.codeborne.selenide.SelenideElement;
 import ru.topacademy.data.DataHelper;
 import static com.codeborne.selenide.Selenide.$;
 
 public class LoginPage {
+    private SelenideElement loginField = $("[data-test-id=login] input");
+    private SelenideElement passwordField = $("[data-test-id=password] input");
+    private SelenideElement button = $("[data-test-id=action-login]");
+
     public VerificationPage validLogin(DataHelper.AuthInfo info) {
-        $("[data-test-id=login] input").setValue(info.getLogin());
-        $("[data-test-id=password] input").setValue(info.getPassword());
-        $("[data-test-id=action-login]").click();
+        loginField.setValue(info.getLogin());
+        passwordField.setValue(info.getPassword());
+        button.click();
         return new VerificationPage();
     }
 }
